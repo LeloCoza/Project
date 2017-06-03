5ce1437c9aa73baea6e34c64a0c631f65e54e047
Zombulelo <nombulelojk@outlook.com>
2017-06-03 23:05:00 +0200

    Add project files.

---
+++ FishLandProj/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace FishLandProj.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ FishLandProj/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FishLandProj.Startup))]
+namespace FishLandProj
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
